f82e27b9c0d787bc87bfe86f8973a7d941545a4f
DevBuike <142910858+DevBuike@users.noreply.github.com>
2025-06-02 21:39:10 +0000

    mplement the User Modal

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,39 +1,60 @@
-// pages/users/index.tsx
-import React from 'react';
-import Header from '@/components/layout/Header';
-import Footer from '@/components/layout/Footer';
-import {UserProps} from '@/interfaces';
-import UserCard from '@/components/common/UserCard';
+import Header from "@/components/layout/Header";
+import UserCard from "@/components/common/UserCard";
+import UserModal from "@/components/common/UserModal";
+import { UserProps, UserData } from "@/interfaces";
 import { useState } from "react";
 
-const Users: React.FC<UserProps> = ({users}: any) => {
-    const [posts ] = useState<UserProps[]>(users);
+const Users: React.FC<UserProps[]> = ({ users }: any) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [posts, setUserList] = useState<UserProps[]>(users);
+
+  const handleAddUser = (newUser: UserData) => {
+    const newUserWithId: UserProps = {
+      ...newUser,
+      id: posts.length + 1,
+    };
+    setUserList([...posts, newUserWithId]);
+  };
+
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <Header />
-      <main className="max-w-4xl mx-auto px-4 py-8">
-        <h1 className="text-2xl font-bold mb-6">Users</h1>
+      <main className="p-4">
+        <div className="flex justify-between mb-4">
+          <h1 className="text-2xl font-semibold">User List</h1>
+          <button
+            onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white"
+          >
+            Add User
+          </button>
+        </div>
         <div className="grid grid-cols-3 gap-4">
           {posts.map((user: UserProps, index: number) => (
             <UserCard key={index} {...user} />
           ))}
         </div>
       </main>
-      <Footer />
-    </>
+
+      {isModalOpen && (
+        <UserModal
+          onClose={() => setModalOpen(false)}
+          onSubmit={handleAddUser}
+        />
+      )}
+    </div>
   );
 };
 
 export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users")
-  const users = await response.json()
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
 
   return {
     props: {
-      users
-    }
-  }
+      users,
+    },
+  };
 }
 
 export default Users;
-
